2660bf773aaeec8e31cccc9e3cc531cb80bd7100
alton15 <alton15@naver.com>
2018-07-31 23:39:35 +0900

    이미지 슬라이드를 위한 main

---
+++ practice/practice9/app/src/main/java/com/example/dsm2018/practice9/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.dsm2018.practice9;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    Adapter adapter;
+    ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.view);
+        adapter = new Adapter(this);
+        viewPager.setAdapter(adapter);
+    }
+}
