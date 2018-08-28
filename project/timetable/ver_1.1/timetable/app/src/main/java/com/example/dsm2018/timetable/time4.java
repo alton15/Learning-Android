0aabfe18858f990628845977dfb70a3a7573fd1b
alton15 <alton15@naver.com>
2018-08-28 20:04:38 +0900

    시간표 프로젝트 ver 1.1 / 1반, 2반, 3반 버튼 효과 적용

---
+++ project/timetable/ver_1.1/timetable/app/src/main/java/com/example/dsm2018/timetable/time4.java
@@ -0,0 +1,12 @@
+package com.example.dsm2018.timetable;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class time4 extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.time4);
+    }
+}
