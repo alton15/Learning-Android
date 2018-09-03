6b3d39549d5dd3683477d31162083988e9eb8a5e
alton15 <alton15@naver.com>
2018-09-03 23:25:01 +0900

    시간표 프로젝트 ver 1.2 / ClassTime.java

---
+++ project/timetable/ver_1.2/timetable/app/src/main/java/com/example/dsm2018/timetable/SecondClassTimeActivity.java
@@ -0,0 +1,13 @@
+package com.example.dsm2018.timetable;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SecondClassTimeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.time2);
+    }
+}
