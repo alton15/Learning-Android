c91c7e0c839f3d152d741928b6452c65d55674ec
alton15 <alton15@naver.com>
2018-10-25 23:30:57 +0900

    firebase를 이용한 회원가입 연습 / 로그인 성공 시 액티비티 전환

---
+++ practice/practice_firebase/email_password_intent/practice_firebase/app/src/main/java/com/example/dsm2018/practice_firebase/Success_page.java
@@ -0,0 +1,13 @@
+package com.example.dsm2018.practice_firebase;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Success_page extends Activity{
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.success_login);
+    }
+
+}
